720ad17b01f76c8d3427c59c6679483aca9a57e4
COOL_TECH <156781377+Jeronmathew44@users.noreply.github.com>
2024-05-10 20:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    # Specify the port number here
-    port = 443  # Example port number for HTTPS traffic
-    app.run(port=port)
+    app.run(port=5000)
